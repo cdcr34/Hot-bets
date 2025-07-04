e243fc4090ab7d9c1d2db4e52eb739091fbc1efd
cdcr34 <cdcr@rocketmail.com>
2025-07-04 13:22:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ roi_decimal = row.iloc[0]['ROI (%)'] / 100
 sample_size = int(row.iloc[0]['Sample Size'])
 
 avg_bet_size_units = row.iloc[0]['Avg Bet Size']
-expected = expected_roi(new_odds, itp)
 
 # --- User inputs odds ---
 original_odds = st.number_input("Original Odds (the odds the bettor got)", value=-100)
